e560523a18798c050d6a324bdf0f30626a2ebfca
pavolbincik <pavolbinck@gmail.com>
2017-07-16 15:25:12 +0200

    change router to https

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -17,7 +17,7 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = new Route('//%host%/<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
 
